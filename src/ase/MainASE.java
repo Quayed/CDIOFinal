dae82032c3223357a43badd771519116648fbdd8
al <a@saloweb.dk>
2015-06-01 14:38:11 +0200

    Mains added

---
+++ src/ase/MainASE.java
@@ -0,0 +1,10 @@
+package ase;
+
+public class MainASE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/weight_simulator/MainSimulator.java
@@ -0,0 +1,5 @@
+package weight_simulator;
+
+public class MainSimulator {
+
+}
