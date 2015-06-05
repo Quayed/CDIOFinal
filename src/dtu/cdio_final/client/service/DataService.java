2f57ceee6c58eed61694cad0de85c208d4f9954b
Mathias Sondrup <matsondrup@gmail.com>
2015-06-05 06:56:37 +0200

    Added two interfaces and a class for the RPCs

---
+++ src/dtu/cdio_final/client/service/DataService.java
@@ -0,0 +1,7 @@
+package dtu.cdio_final.client.service;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+
+public interface DataService extends RemoteService{
+
+}
---
+++ src/dtu/cdio_final/server/service/DataServiceImpl.java
@@ -0,0 +1,9 @@
+package dtu.cdio_final.server.service;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+import dtu.cdio_final.client.service.DataService;
+
+public class DataServiceImpl extends RemoteServiceServlet implements DataService{
+
+}
