c627b6d8ff8a5666e7e20126ae5e14e0b5a4cc95
Cat <Cat@Cat-PC>
2015-06-06 21:20:10 +0200

    første pass over services, har kun lavet det simple slavearbejde. har også uploaded password manageren (til fieldverification) og Roni's token handler.

--- src/dtu/cdio_final/server/service/DataServiceImpl.java
+++ src/dtu/cdio_final/server/service/DataServiceImpl.java
@@ -5,6 +5,19 @@ import java.util.List;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import dtu.cdio_final.client.service.DataService;
+import dtu.cdio_final.server.dal.daoimpl.FormulaCompDAO;
+import dtu.cdio_final.server.dal.daoimpl.FormulaDAO;
+import dtu.cdio_final.server.dal.daoimpl.MaterialBatchDAO;
+import dtu.cdio_final.server.dal.daoimpl.MaterialDAO;
+import dtu.cdio_final.server.dal.daoimpl.ProductbatchDAO;
+import dtu.cdio_final.server.dal.daoimpl.UserDAO;
+import dtu.cdio_final.server.dal.daointerfaces.DALException;
+import dtu.cdio_final.server.dal.daointerfaces.IFormulaCompDAO;
+import dtu.cdio_final.server.dal.daointerfaces.IFormulaDAO;
+import dtu.cdio_final.server.dal.daointerfaces.IMaterialBatchDAO;
+import dtu.cdio_final.server.dal.daointerfaces.IMaterialDAO;
+import dtu.cdio_final.server.dal.daointerfaces.IProductbatchDAO;
+import dtu.cdio_final.server.dal.daointerfaces.IUserDAO;
 import dtu.cdio_final.shared.dto.FormulaCompDTO;
 import dtu.cdio_final.shared.dto.FormulaDTO;
 import dtu.cdio_final.shared.dto.MaterialDTO;
@@ -12,102 +25,244 @@ import dtu.cdio_final.shared.dto.MaterialbatchDTO;
 import dtu.cdio_final.shared.dto.ProductbatchDTO;
 import dtu.cdio_final.shared.dto.UserDTO;
 
-public class DataServiceImpl extends RemoteServiceServlet implements DataService{
+@SuppressWarnings("serial")
+public class DataServiceImpl extends RemoteServiceServlet implements DataService
+{
+	private final IUserDAO userDao = new UserDAO();
+	private final IMaterialDAO materialDao = new MaterialDAO();
+	private final IFormulaDAO formulaDao = new FormulaDAO();
+	private final IFormulaCompDAO formulaCompDao = new FormulaCompDAO();
+	private final IMaterialBatchDAO materialBatchDao = new MaterialBatchDAO();
+	private final IProductbatchDAO productBathcDao = new ProductbatchDAO();
+	
+	@Override
+	public void createUser(UserDTO user)
+	{
+		try
+		{
+			userDao.createUser(user);
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+		}
+	}
 
 	@Override
-	public void createUser(UserDTO user) {
-		// TODO Auto-generated method stub
+	public List<UserDTO> getUsers()
+	{
+		List<UserDTO> result = null; 
+		try
+		{
+			result = userDao.getUserList();
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public void updateUser(UserDTO user)
+	{
+		try
+		{
+			userDao.updateUser(user);
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void deleteUser(int userID)
+	{
+		//NYI missing "active/inactive" field in DAO/DTO
 		
+//		try
+//		{
+//			UserDTO user = userDao.getUser(userID);
+//			user.setActiveStatus("inactive");
+//			userDao.updateUser(user);
+//		}
+//		catch (DALException e)
+//		{
+//			e.printStackTrace();
+//		}
 	}
 
 	@Override
-	public List<UserDTO> getUsers() {
-		// TODO Auto-generated method stub
-		return null;
+	public void createMaterial(MaterialDTO material)
+	{
+		try
+		{
+			materialDao.createMaterial(material);
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public void updateUser(UserDTO user) {
-		// TODO Auto-generated method stub
-		
+	public List<MaterialDTO> getMaterials()
+	{
+		List<MaterialDTO> result = null; 
+		try
+		{
+			result = materialDao.getMaterialList();
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	@Override
-	public void deleteUser(int userID) {
-		// TODO Auto-generated method stub
-		
+	public void updateMaterial(MaterialDTO material)
+	{
+		try
+		{
+			materialDao.updateMaterial(material);
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public void createMaterial(MaterialDTO material) {
-		// TODO Auto-generated method stub
-		
+	public void createFormula(FormulaDTO formula)
+	{
+		try
+		{
+			formulaDao.createFormula(formula);
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public List<MaterialDTO> getMaterials() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<FormulaDTO> getFormulas()
+	{
+		List<FormulaDTO> result = null; 
+		try
+		{
+			result = formulaDao.getFormulaList();
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	@Override
-	public void updateMaterial(MaterialDTO material) {
-		// TODO Auto-generated method stub
-		
+	public void createFormulaComp(FormulaCompDTO formulaComp)
+	{
+		try
+		{
+			formulaCompDao.createFormulaComp(formulaComp);
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public void createFormula(FormulaDTO formula) {
-		// TODO Auto-generated method stub
-		
+	public List<FormulaCompDTO> getFormulaComps(int formulaID)
+	{
+		List<FormulaCompDTO> result = null; 
+		try
+		{
+			result = formulaCompDao.getFormulaCompList();
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	@Override
-	public List<FormulaDTO> getFormulas() {
-		// TODO Auto-generated method stub
-		return null;
+	public void createMaterialBatch(MaterialbatchDTO materialBatch)
+	{
+		try
+		{
+			materialBatchDao.createMaterialBatch(materialBatch);
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public void createFormulaComp(FormulaCompDTO formulaComp) {
-		// TODO Auto-generated method stub
-		
+	public List<MaterialbatchDTO> getMaterialBatches()
+	{
+		List<MaterialbatchDTO> result = null; 
+		try
+		{
+			result = materialBatchDao.getMaterialBatchList();
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	@Override
-	public List<FormulaCompDTO> getFormulaComps(int formulaID) {
-		// TODO Auto-generated method stub
-		return null;
+	public void createProductBatch(ProductbatchDTO productBatch)
+	{
+		try
+		{
+			productBathcDao.createProductbatch(productBatch);
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public void createMaterialBatch(MaterialbatchDTO materialBatch) {
-		// TODO Auto-generated method stub
-		
+	public List<ProductbatchDTO> getProductBatches()
+	{
+		List<ProductbatchDTO> result = null; 
+		try
+		{
+			result = productBathcDao.getProductbatchList();
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	@Override
-	public List<MaterialbatchDTO> getMaterialBatches() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void createProductBatch(ProductbatchDTO productBatch) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public List<ProductbatchDTO> getProductBatches() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean login(int userID, String password) {
-		// TODO Auto-generated method stub
+	public boolean login(int userID, String password)
+	{
+		//TODO: update to use tokens
+		//TODO: update to use hashing
+		try
+		{
+			UserDTO user = userDao.getUser(userID);
+			
+			if(user.getPassword().equals(password))
+				return true;
+		}
+		catch (DALException e)
+		{
+			System.err.println("Invalid login or password");
+			e.printStackTrace();
+		}
 		return false;
 	}
-
 }
---
+++ src/dtu/cdio_final/shared/PasswordManager.java
@@ -0,0 +1,183 @@
+package dtu.cdio_final.shared;
+
+import java.nio.ByteBuffer;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+import java.util.Random;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+import org.apache.xerces.impl.dv.util.Base64;
+
+public class PasswordManager
+{
+	//Class variables
+	private static Random rand = new Random();
+	private static final String[] ALPHABET = {"0123456789", 
+		"abcdefghijklmnopqrstuvwxyz", 
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ", 
+	".+-_?=!"};
+	private static final int PASSWORD_MIN = 6;
+	private static final int HASH_BYTE_SIZE = 64; // 512 bits
+	private static final int PBKDF2_ITERATIONS = 1000;
+	
+	//-----------------------------------------------------------------------------------------------------------
+	//Public Methods
+	//-----------------------------------------------------------------------------------------------------------
+	
+	/**
+	 * Generates a random password, matching the needed criteria, and with length 8
+	 * @return The randomly generated password.
+	 */
+	public static String generatePassword()
+	{	
+		return generatePasswordRekursive(rand.nextInt(ALPHABET.length), PASSWORD_MIN + 2, "", 2);
+	}
+
+	/**
+	 * Checks if the given password matches the criteria
+	 * @param pass The password to check
+	 * @return true if the password matches 3 of 4 categories, and has a length >= 6, false otherwise.
+	 */
+	public static boolean checkPassword(String pass)
+	{
+		boolean[] check = checkPW(pass);
+
+		int passedCategories = 0;
+		for (int i = 0; i < 4; i++)
+			if(check[i])
+				passedCategories++;
+
+		if(passedCategories > 2 && check[4])
+			return true;
+
+		return false;
+	}
+	
+	//-----------------------------------------------------------------------------------------------------------
+	//Private Methods
+	//-----------------------------------------------------------------------------------------------------------
+	
+	/**
+	 * checks if the password matches the requirements, returns a boolean array
+	 *  of length = 5 where:
+	 * 0 = [0-9]
+	 * 1 = [a-z]
+	 * 2 = [A-Z]
+	 * 3 = [\\+\\-_?=]
+	 * 4 = length >= 6
+	 * @param pw The password to check
+	 * @return An array containing true for all passed test, and false for all failed ones.
+	 */
+	private static boolean[] checkPW(String pw)
+	{
+		boolean[] results = new boolean[5];
+		String[] patterns = new String[4];
+
+		//make a pattern for each category
+		patterns[0] = ".*\\d+.*";
+		patterns[1] = ".*[a-z]+.*";
+		patterns[2] = ".*[A-Z]+.*";
+		patterns[3] = ".*[\\+\\-_?=!\\.]+.*";
+
+		//check against patterns and store results of check
+		for (int i = 0; i < patterns.length; i++)
+			results[i] = pw.matches(patterns[i]);
+		
+		//check length of pw
+		results[4] = pw.length() >= PASSWORD_MIN;
+
+		return results;
+	}
+
+	/**
+	 * 
+	 * @param cat a number marking what category to pull from
+	 * @param max the max length of the password
+	 * @param pass the password anything added here will be appended to the start of the pass
+	 * @param catCount number of categories that we still need to fill, minus the current one.
+	 * @return A string containing a random password.
+	 */
+	private static String generatePasswordRekursive(int cat, int max, String pass, int catCount)
+	{
+		//if we dont fill the required number of categories, or if the password is too short:
+		if(catCount > 0 || pass.length() < max)
+		{
+			//figure out how many chars we should max pick in this iteration
+			int limit = max - pass.length() - (catCount);
+			//select a random number between 0 and that limit
+			int random = rand.nextInt(limit + 1);
+			//if the number is zero we skip the category that we are currently in, if not:
+			if(random > 0)
+			{
+				//pick a random char from the current category, and add it to the password,
+				//repeat till we reach the random limit we picked. 
+				for (int i = 0; i < random; i++)
+					pass += ALPHABET[cat].charAt(rand.nextInt(ALPHABET[cat].length()));
+				//go to next category
+				cat = (cat + 1) % 4;
+				//we picked more than zero chars from the category, so the number of missing categories
+				//is reduced by 1, unless we already cover more than we need and are just filling in password length.
+				if(catCount != 0)
+					catCount--;
+			}
+			//recursion untill we no longer hit this if-statement
+			pass = generatePasswordRekursive(cat, max, pass, catCount);
+		}
+		//the password should now be valid, so return.
+		return pass;
+	}
+
+	
+	
+
+	//add code to generate salt (server)
+	private static byte[] generateSalt(int nrOfBits)
+	{	
+		Random rand = new SecureRandom();
+		byte[] salt = new byte[nrOfBits];
+		rand.nextBytes(salt);
+		
+		return salt;
+	}
+	
+	//add code to generate hash from pass and salt (server and client)
+	private static byte[] generateHash(String pass, byte[] salt)
+	{
+		try
+		{
+			SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+			PBEKeySpec keySpec = new PBEKeySpec(pass.toCharArray(), salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE * 8);
+			SecretKey digest = skf.generateSecret(keySpec);
+			
+			return digest.getEncoded();
+		}
+		catch (NoSuchAlgorithmException e)
+		{
+			e.printStackTrace();
+		}
+		catch (InvalidKeySpecException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	//add code to compare hashes (server)
+
+	//add code to encrypt and decrypt base64 (server and client)
+
+	//kun til tests, slettes før endelig version.
+	public static void main(String[] args)
+	{
+		byte[] results = generateHash("hej", generateSalt(512));
+		for (int i = 0; i < results.length; i++)
+			System.out.println(i + "  " + Integer.toString(results[i] & 0xff,2));
+		
+	}
+
+}
