33dc85ac0b413017897adf0746f291d7046d198c
al <a@saloweb.dk>
2015-06-15 14:40:32 +0200

    Added next material to dao

--- src/dtu/cdio_final/server/dal/daoimpl/ProductbatchDAO.java
+++ src/dtu/cdio_final/server/dal/daoimpl/ProductbatchDAO.java
@@ -9,6 +9,7 @@ import java.util.List;
 import dtu.cdio_final.server.dal.connector.Connector;
 import dtu.cdio_final.server.dal.daointerfaces.DALException;
 import dtu.cdio_final.server.dal.daointerfaces.IProductbatchDAO;
+import dtu.cdio_final.shared.dto.MaterialDTO;
 import dtu.cdio_final.shared.dto.ProductbatchDTO;
 
 public class ProductbatchDAO implements IProductbatchDAO {
@@ -42,6 +43,31 @@ public class ProductbatchDAO implements IProductbatchDAO {
 			throw new DALException(e);
 		}
 		return list;
+	}
+	
+	@Override
+	public MaterialDTO getNextMaterial(int pbID) throws DALException{
+		try {
+			PreparedStatement ps = Connector.prepare("SELECT min(m.material_id) material_id, m.material_name, m.provider from productbatch pb "+
+													"inner join formula f on pb.formula_id = f.formula_id "+
+													"inner join formula_component fc on f.formula_id = fc.formula_id "+
+													"inner join material m on fc.material_id = m.material_id "+
+													"Where pb.pb_id = ? AND fc.material_id not in (select mb.material_id from productbatch pb "+
+													"inner join productbatch_component pbc on pb.pb_id = pbc.pb_id "+
+													"inner join materialbatch mb on pbc.mb_id = mb.mb_id "+
+													"Where pb.pb_id = ?)");
+			ps.setInt(1, pbID);
+			ps.setInt(2, pbID);
+			ResultSet rs = ps.executeQuery();
+			if (!rs.first()) {
+				return null;
+			} else {
+				return new MaterialDTO(rs.getInt("material_id"), rs.getString("material_name"), rs.getString("provider"));
+			}
+		} catch (SQLException e) {
+			throw new DALException(e);
+		}
+
 	}
 
 	@Override
